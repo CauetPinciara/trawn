06dd15a8971a7b6a852ea70aaad8772f1e57cd0c
CauetPinciara <cauetpinciara@gmail.com>
2024-03-14 00:50:07 -0300

    prisma setup

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,12 +1,17 @@
 import Navigation from "@/components/site/navigation";
 import { ReactNode } from "react";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="h-full">
-      <Navigation />
-      {children}
-    </div>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <main className="h-full">
+        <Navigation />
+        {children}
+      </main>
+    </ClerkProvider>
   );
 };
 
